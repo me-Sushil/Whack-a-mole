bfa79d14523f9b8e13eac6659ae8a046d090a9ab
me-Sushil <girbahadurbk123@gmail.com>
2025-07-28 11:32:02 +0545

    add eventlistener

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let score = 0;
 
 let holes = document.querySelectorAll(".hole");
+
+setInterval(function (){
+  let randomNum = Math.floor(Math.random() * holes.length);
+  holes[randomNum].classList.toggle("mole");
+}, 450);
+
+let whackamole = document.getElementById("whack-a-mole");
+
+whackamole.addEventListener("click", (event)=>{
+    
+
+})
