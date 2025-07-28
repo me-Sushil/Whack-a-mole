d26d348bedf4780d5bd345408b499e491db386e5
me-Sushil <girbahadurbk123@gmail.com>
2025-07-28 12:08:22 +0545

    add miss element

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ let holes = document.querySelectorAll(".hole");
 setInterval(function (){
   let randomNum = Math.floor(Math.random() * holes.length);
   holes[randomNum].classList.toggle("mole");
-}, 450);
+}, 750);
 
+let missnum = 0;
 let whackamole = document.getElementById("whack-a-mole");
-
+let scoreCount = document.getElementById("score");
+let miss = document.getElementById("miss");
 whackamole.addEventListener("click", (event)=>{
-    
-
+    if(event.target.matches(".mole")){
+        event.target.classList.remove("mole");
+        score++;
+        scoreCount.innerText = `Score : ${score}`;
+    }else{
+        missnum++;
+        miss.innerText = `Miss : ${missnum}`;
+    }
 })
